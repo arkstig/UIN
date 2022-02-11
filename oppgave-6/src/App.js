62420dd977a1f2531c84ea5ffe0523f2d143e35b
Stig Ark <stig.ark@gmail.com>
2022-02-11 11:07:08 +0100

    sa

--- oppgave-6/src/App.js
+++ oppgave-6/src/App.js
@@ -1,10 +1,19 @@
 import './styles.css';
 
+function Header() {
+  return (
+    <header>
+        
+            <h1>HIOF</h1>
+            <nav>Stig Ark</nav>
+        
+    </header>
+  )
+}
+
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <Header />
   );
 }
 
