7f2fc36baa6dcf7d9013ba9d13abf31075699c6b
Stig Ark <stig.ark@gmail.com>
2022-02-11 11:25:00 +0100

    test

--- oppgave-6/src/App.js
+++ oppgave-6/src/App.js
@@ -11,9 +11,59 @@ function Header() {
   )
 }
 
+function Form() {
+  return (
+    <section id="section1">
+    <form>
+        <label for="title"><h2>Title</h2></label>
+        <input type="text" id="title"/>
+        <label for="content"><h2>Content</h2></label>
+        <textarea id="content" rows="6" cols="40"></textarea>
+        <button id="formbutton" type="submit" value="submit">Add</button>
+    </form>
+</section>
+  )
+}
+
+function TodoList(){
+  return (
+<section id="todo">
+            
+        <Todo />
+        <Todo />
+        <Todo />
+        <Todo />
+        <Todo />
+        <Todo />
+        </section>
+  )
+}
+
+function Todo() {
+  return (
+    <article>
+                <h3>Todotitle</h3>
+                <p>Lorem ipsum dolor sit amet. Paragraph Todo...</p>
+                <Button />
+            </article>
+  )
+}
+
+function Button() {
+  return (
+    <button id="complete">Complete</button>
+  )
+}
+
 export default function App() {
   return (
+    <>
     <Header />
+    <main>
+    <Form />
+    <TodoList />
+    </main>
+    </>
   );
 }
 
