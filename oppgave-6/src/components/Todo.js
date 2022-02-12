2235307ab0c6de10b600a7a2358f2e59feb4859d
Stig Ark <stig.ark@gmail.com>
2022-02-12 16:28:54 +0100

    wadw

--- oppgave-6/src/components/Todo.js
+++ oppgave-6/src/components/Todo.js
@@ -1,18 +1,11 @@
-
-export default function Todo({id, title, content, todo, removeTodo }) {
+export default function Todo({ id, title, content, todo, removeTodo }) {
   return (
-    
-      <article>
-        <h3>{title}</h3>
-        <p>{content}</p>
-        <button
-          className="remove"
-          type="button"
-          onClick={() => removeTodo(id)}
-        >
-          Utført
-        </button>
-      </article>
-    
+    <article>
+      <h3>{title}</h3>
+      <p>{content}</p>
+      <button className="remove" type="button" onClick={() => removeTodo(id)}>
+        Utført
+      </button>
+    </article>
   );
 }
